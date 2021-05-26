719e4efe4185f8ee57568cb0130a3fba7831bc92
Akihiko Kusanagi <nagi@nagi-p.com>
2021-05-27 00:47:48 +0800

    Fix script errors

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function get_version {
-    if [ "$TRAVIS_BRANCH" =~ ^release.*$ ]; then
+    if [[ "$TRAVIS_BRANCH" =~ ^release.*$ ]]; then
         # Travis executes this script from the repository root, so at the same level than package.json
         VERSION=$(node -p -e "require('./package.json').version")
         if [[ "$VERSION" =~ -(alpha|beta|rc) ]]; then
@@ -9,7 +9,7 @@ function get_version {
         else
         	echo $VERSION
         fi
-    elif [[ "$TRAVIS_BRANCH" == "master" ]]; then
+    elif [ "$TRAVIS_BRANCH" == "master" ]; then
         echo "master"
     else
         echo ""
