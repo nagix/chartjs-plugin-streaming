9ff78794af95cac3a7e7e9d54f562005c0f04a9f
Akihiko Kusanagi <nagi@nagi-p.com>
2021-05-31 20:32:11 +0800

    Add typescript support

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,48 @@
-import {Plugin, Scale, registerables} from 'chart.js';
+import { Chart, ChartComponent, ChartType, TimeScale, TimeScaleOptions, Plugin } from 'chart.js';
+
+interface Options {
+  duration?: number;
+  delay?: number;
+  frameRate?: number;
+  refresh?: number;
+  onRefresh?: (this: RealTimeScale, chart: Chart) => void | null;
+  pause?: boolean;
+  ttl?: number;
+}
+
+export type RealTimeScaleOptions = TimeScaleOptions & {
+  realtime: Options;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface RealTimeScale<O extends RealTimeScaleOptions = RealTimeScaleOptions> extends TimeScale<O> {}
+
+export const RealTimeScale: ChartComponent & {
+  prototype: RealTimeScale;
+  new <O extends RealTimeScaleOptions = RealTimeScaleOptions>(cfg: Record<string, unknown>): RealTimeScale<O>;
+};
+
+declare module 'chart.js' {
+  interface CartesianScaleTypeRegistry {
+    realtime: {
+      options: RealTimeScaleOptions;
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface PluginOptionsByType<TType extends ChartType> {
+    /**
+     * Per chart streaming plugin options.
+     */
+    streaming?: Options;
+  }
+
+  enum UpdateModeEnum {
+    quiet = 'quiet'
+  }
+}
+
+declare const registerables: ChartComponent[];
 
 export const StreamingPlugin: Plugin;
-export const RealTimeScale: Scale;
 export default registerables;
