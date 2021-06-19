2d8abd9a14602272ab5c038eda31aebc21493e3e
Akihiko Kusanagi <nagi@nagi-p.com>
2021-06-19 14:49:44 +0800

    Correct docs config script

--- scripts/docs-config.sh
+++ scripts/docs-config.sh
@@ -8,4 +8,4 @@ VERSION=$(node -p -e "require('./package.json').version")
 MODE=$1
 TAG=$(tag_from_version "$VERSION" "$MODE")
 
-sed -i -e "s/VERSION/$TAG/g" "docs/.vuepress/config.js"
+sed -i -e "s/\"VERSION\"/\"$TAG\"/g" "docs/.vuepress/config.js"
